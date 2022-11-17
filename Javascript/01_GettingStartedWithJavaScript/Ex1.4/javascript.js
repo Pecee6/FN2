365fca1ed0ce85fbc3d02802f7cc11c50c205e62
Pecee6 <bogoevskipetar99@gmail.com>
2022-11-17 20:38:59 +0100

    JavaScript Vezbi

---
+++ Javascript/01_GettingStartedWithJavaScript/Ex1.4/javascript.js
@@ -0,0 +1,5 @@
+let a = 10;
+// set value for a
+console.log (a);
+// print value for a
+
---
+++ Javascript/01_GettingStartedWithJavaScript/Ex1.5/javascript.js
@@ -0,0 +1,26 @@
+// console.log ("Petar");
+
+/*
+Multi
+Line
+Comment
+*/
+
+/*
+let userName = prompt("Insert your name!");
+alert("Vaseto ime " + userName + " e procesirano!");
+console.log(userName);
+*/
+
+
+let randomNum = Math.random() * 3;
+console.log("My random number: " + randomNum);
+let roundNum = Math.floor(randomNum) + 1;
+console.log("My rounded number: " + roundNum);
+let num = prompt("Guess a number from 1 to 3");
+
+if (roundNum == num) {
+    alert("You guessed right!");
+} else {
+    alert("You guessed wrong");
+}
