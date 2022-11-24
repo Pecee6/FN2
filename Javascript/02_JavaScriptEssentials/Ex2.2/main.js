1c3520a5ba0627e5644bd1f5e9b3c265bac62ea8
Pecee6 <bogoevskipetar99@gmail.com>
2022-11-24 18:06:15 +0100

    javascript

---
+++ Javascript/02_JavaScriptEssentials/Ex2.2/main.js
@@ -0,0 +1,6 @@
+let myName = "Petar";
+let age = "20";
+let canCode = "true";
+
+const str = 'Hello, my name is ' + myName + ', I am ' + age + ' years old and I can code JavaScript: ' + canCode + '.';
+console.log(str);
