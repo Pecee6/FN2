f97a810b74af52026ed19eae5629bdf2c559bb1a
Pecee6 <bogoevskipetar99@gmail.com>
2022-11-24 19:28:34 +0100

    Ex 2.4 new file:   Javascript/02_JavaScriptEssentials/Ex2.4/index.html new file:   Javascript/02_JavaScriptEssentials/Ex2.4/main.js

---
+++ Javascript/02_JavaScriptEssentials/Ex2.4/main.js
@@ -0,0 +1,16 @@
+let a = 100;
+console.log (a);
+
+let b = 20;
+console.log (b);
+
+let c = 20;
+console.log (c);
+
+console.log (a += b);
+
+console.log (a /= c);
+
+console.log (c %= b);
+
+console.log (c);
