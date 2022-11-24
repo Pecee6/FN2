50fef1d722797d3bc1be4bf91f9738ca025187dc
Pecee6 <bogoevskipetar99@gmail.com>
2022-11-24 19:03:25 +0100

    Pitagorova teorema new file:   Javascript/02_JavaScriptEssentials/Ex2.3/index.html new file:   Javascript/02_JavaScriptEssentials/Ex2.3/main.js

---
+++ Javascript/02_JavaScriptEssentials/Ex2.3/main.js
@@ -0,0 +1,14 @@
+alert ("Pitagorova teorema");
+
+let a = prompt ("Vnesi vrednost za a");
+console.log(a);
+
+let b = prompt ("Vnesi vrednost za b");
+console.log(b);
+
+a = Math.pow(a,2);
+b = Math.pow(b,2);
+
+c = a + b;
+c = Math.sqrt(c);
+console.log(c);
