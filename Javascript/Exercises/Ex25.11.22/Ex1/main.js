066c0c80a3eef5153d0747fa77a0ef2f09599e3c
Pecee6 <bogoevskipetar99@gmail.com>
2022-11-26 19:13:59 +0100

    ex1 new file:   Javascript/Exercises/Ex25.11.22/Ex1/index.html new file:   Javascript/Exercises/Ex25.11.22/Ex1/main.js

---
+++ Javascript/Exercises/Ex25.11.22/Ex1/main.js
@@ -0,0 +1,37 @@
+let  potato = "potato";
+console.log(typeof(potato));
+
+let number = 15;
+console.log(typeof(number));
+
+let number1= 5.5;
+console.log(typeof(number1));
+
+console.log(typeof(NaN));
+
+let hello = "hello";
+console.log(typeof(hello));
+
+console.log(typeof(true));
+
+let broj = (1 != 2);
+console.log(typeof(broj));
+
+let hamburger = "hamburger" + "s";
+console.log (typeof(hamburger));
+
+let hamburgers = "hamburger" - "s";
+console.log (typeof(hamburgers));
+
+let num = "1" + "3" ;
+console.log (typeof(num));
+
+let num1 = "1" - "3";
+console.log(typeof(num1));
+
+let johnny = "johnny" + 5;
+console.log(typeof(johnny));
+
+
+let johnny1 = "johnny" - 5;
+console.log(typeof(johnny1));
