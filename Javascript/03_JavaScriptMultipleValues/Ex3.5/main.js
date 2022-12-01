3915e7d35eccf7f49a6663d91576e47885659fc7
Pecee6 <bogoevskipetar99@gmail.com>
2022-12-01 20:29:32 +0100

    Ex3.5 new file:   Javascript/03_JavaScriptMultipleValues/Ex3.5/index.html new file:   Javascript/03_JavaScriptMultipleValues/Ex3.5/main.js

---
+++ Javascript/03_JavaScriptMultipleValues/Ex3.5/main.js
@@ -0,0 +1,24 @@
+let people = {
+    friends: []
+};
+
+let petar = {
+    firstName: "Petar",
+    lastName: "Bogoevski",
+    ID: 1
+};
+
+let miki = {
+    firstName: "Miki",
+    lastName: "Stojkovski",
+    ID: 2
+};
+
+let martin = {
+    firstName: "Martin",
+    lastName: "Martinovski",
+    ID : 3
+};
+
+people.friends.push(petar, miki, martin);
+console.log (people);
