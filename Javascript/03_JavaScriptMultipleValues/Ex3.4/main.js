a05d6203d7582dcd4ad7744d6c37722bf9f5dd47
Pecee6 <bogoevskipetar99@gmail.com>
2022-12-01 19:07:26 +0100

    ex3.4

---
+++ Javascript/03_JavaScriptMultipleValues/Ex3.4/main.js
@@ -0,0 +1,17 @@
+let myCar = {
+    make: "Ford",
+    model: "Fiesta",
+    year: 2012,
+    color: "green"
+};
+console.log("MyCar color: ",  myCar.color);
+
+let prop = "color";
+myCar[prop] = "red";
+console.log("MyCar new color: ",  myCar.color);
+
+prop = "forSale";
+myCar[prop] = true;
+
+console.log("Car" , myCar.make, myCar.model , my Car.year);
+console.log("For sale: ", myCar.forSale ? "Yes" : "No");
